f283f93bcfa3aee2d3a7ff0587e919672e8770ec
myzhulin <1320291471@qq.com>
2019-08-02 09:10:44 +0800

    1.修改jackson的依赖版本，并将redis改为依赖本地服务

--- src/main/java/com/zl/controller/RedisController.java
+++ src/main/java/com/zl/controller/RedisController.java
@@ -3,7 +3,6 @@ package com.zl.controller;
 import com.zl.pojo.UserDO;
 import com.zl.service.RedisUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
