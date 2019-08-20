7b7f0ddbbb24d82be00182c0545f2dcb7b4531e0
myzhulin <1320291471@qq.com>
2019-08-20 13:37:12 +0800

    优化导包

--- src/main/java/com/zl/aop/SystemLogAspect.java
+++ src/main/java/com/zl/aop/SystemLogAspect.java
@@ -12,6 +12,7 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.NamedThreadLocal;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
